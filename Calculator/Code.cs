1d16360a69d348da7af6601939188f7e81f53798
steam deck <steam deck@DESKTOP-MHUF9E8>
2025-02-17 00:18:05 +0100

    Ajoutez des fichiers projet.

---
+++ Calculator/Code.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Code
+    {
+        public static void Main(String[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("======================");
+                Console.WriteLine("      Calculator      ");
+                Console.WriteLine("======================");
+                int a = readint("First number: ");
+                Console.WriteLine("Choose action: +, -, *, /, %");
+                String action = Console.ReadLine();
+                int b = readint("Second number: ");
+
+                switch (action)
+                {
+                    case "+":
+                        add(a, b);
+                        break;
+                    case "-":
+                        sub(a, b);
+                        break;
+                    case "*":
+                        mul(a, b);
+                        break;
+                    case "/":
+                        div(a, b);
+                        break;
+                    case "%":
+                        modulo(a, b);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong command");
+                        break;
+                }
+
+                if (action == "exit")
+                {
+                    break;
+                }
+            }
+
+            static int readint(String a)
+            {
+                int result;
+                try
+                {
+                    Console.WriteLine(a);
+                    return result = (int)Convert.ToInt64(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    return 0;
+                }
+            }
+
+            static void add(int a, int b)
+            {
+                Console.WriteLine(a + b);
+            }
+
+            static void sub(int a, int b)
+            {
+                Console.WriteLine(a - b);
+            }
+
+            static void mul(int a, int b)
+            {
+                Console.WriteLine(a * b);
+            }
+
+            static void div(int a, int b)
+            {
+                try
+                {
+                    Console.WriteLine(a / b);
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                }
+            }
+
+            static void modulo(int a, int b)
+            {
+                try
+                {
+                    Console.WriteLine(a % b);
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                }
+            }
+        }
+    }
+}
